31757681833402b9f2013415c1873c4367c328d3
Radhakrishnan J <radhakrishnan.j@himalayawellness.com>
2017-05-19 09:07:29 +0000

    added 1.0.23

--- Institution/components/supportView/index.js
+++ Institution/components/supportView/index.js
@@ -15,12 +15,13 @@
             //TechSupportMobile
         },
         afterShow: function () {
+            $('#span_appversion').html(app.constants.appversion);
             var user = JSON.parse(localStorage.getItem("userdata"));
             var TechSupportEmail = user.TechSupportEmail;
             var TechSupportMobile = user.TechSupportMobile;
             //$('#anchartechsupportemail').html(TechSupportEmail.split(":")[1].split("?")[0]);
             //$('#anchartechsupportmobile').html(TechSupportMobile.split(":")[1]);
-            $('#anchartechsupportemail').attr("href", TechSupportEmail + "&subject=ETHOS-INS App Queries");
+            $('#anchartechsupportemail').attr("href", TechSupportEmail + "&subject=" + app.constants.appname + " App Queries");
             $('#anchartechsupportmobile').attr("href", TechSupportMobile);
         },
     });
